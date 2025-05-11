50a50c423b3a58e8f0cacadad2dce321e6850411
Kelthur <adam_drozdz0@o2.pl>
2025-05-11 13:45:42 +0200

    added view of recent transactions, and CompanyView for anonymous users

--- app/src/main/java/pl/gpwpoid/origin/ui/views/CompaniesListView.java
+++ app/src/main/java/pl/gpwpoid/origin/ui/views/CompaniesListView.java
@@ -6,7 +6,6 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-import jakarta.annotation.security.RolesAllowed;
 import pl.gpwpoid.origin.repositories.views.CompanyListItem;
 import pl.gpwpoid.origin.services.CompanyService;
 
